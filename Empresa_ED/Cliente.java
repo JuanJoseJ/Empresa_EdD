77cb5f85bb5e92b0d268b2f631e5fb8ee1fd2b6a
JuanJoseJ <41352311+JuanJoseJ@users.noreply.github.com>
2019-02-14 20:44:45 -0500

    Create Cliente.java

---
+++ Empresa_ED/Cliente.java
@@ -0,0 +1,25 @@
+package Empresa_ED;
+
+public class CLiente{
+	private String codigo;
+	private String nombre;
+	private String direccion;
+	private String Forma_De_Pago;
+	private String email;
+	private boolean Estado_VIP;
+	private Solicitud[] solicitudes;
+
+	public CLiente(String codigo){
+		this.codigo=codigo;
+	}
+
+	public void Crear_Solicitud(){
+
+	}
+
+	public int Costo_Solicitud(){
+
+	}
+
+	
+}
