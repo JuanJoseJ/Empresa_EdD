b1ad418b6d3496a617bd5318c349c4ecd53636b2
JuanJoseJ <41352311+JuanJoseJ@users.noreply.github.com>
2019-02-14 20:36:09 -0500

    Update Empresa.java

--- Empresa_ED/Empresa.java
+++ Empresa_ED/Empresa.java
@@ -1,5 +1,26 @@
 package Empresa_ED;
 
 public class Empresa {
+	private String nombre;
+	private String direccion;
+	private Pieza[] piezas;
+	private Cliente[] clientes;
+	private Solicitud[]	solicitud; //este lo pongo, porque en la correccion de los errores, ponen que nos faltan estos :)
 
+	public Empresa(String nombre, String direccion){
+		this.nombre=nombre;
+		this.direccion=direccion;
+	}
+
+	public int Costo_Total(){
+
+	}
+
+	public void Crear_Cliente(){
+
+	}
+
+	public void Fabricar_Pieza(){
+		
+	}
 }
