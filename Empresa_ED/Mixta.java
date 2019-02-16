9429df89c04ffe4016512bb240294cf24f1cad5a
jeissongarciacano <47406585+jeissongarciacano@users.noreply.github.com>
2019-02-16 16:53:31 -0500

    Update Mixta.java

--- Empresa_ED/Mixta.java
+++ Empresa_ED/Mixta.java
@@ -19,4 +19,15 @@ public class Mixta extends Pieza{
 		return this.costo;
 	}
 	
-}
+} 
+//constructor incluyendo el arreglo
+/*public Mixta(double peso, String codigo, Pieza piezas[]){
+		super(peso,codigo);
+		double acum=0;;
+		for(int i=0; i<piezas.length;i++) {
+			acum+= piezas[i].getCosto();
+		}
+		this.peso=acum;
+		this.piezas=piezas;
+	
+	}*/
