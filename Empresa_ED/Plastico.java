5e56d6a68468018237f88c4a9b2f8113a45aeca6
beruon <47423301+beruon@users.noreply.github.com>
2019-02-17 08:17:46 -0500

    Update Plastico.java

--- Empresa_ED/Plastico.java
+++ Empresa_ED/Plastico.java
@@ -9,4 +9,8 @@ public class Plastico extends Pieza{
 	public double costo() {
 		return 1.3*peso+30;
 	}	
+	
+	public double getCosto() {    // se va a necesitar despues para calcular el costo por solicitud
+		return this.costo;
+	}
 }
