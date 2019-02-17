d92f105aad6a8f0acf570fc15a84f9c3b0b1687b
beruon <47423301+beruon@users.noreply.github.com>
2019-02-17 08:19:30 -0500

    Update Mixta.java

--- Empresa_ED/Mixta.java
+++ Empresa_ED/Mixta.java
@@ -25,6 +25,9 @@ public class Mixta extends Pieza{
 		}
 		return acum;
 	}
+	public double getCosto() {    // se va a necesitar despues para calcular el costo por solicitud
+		return this.costo;
+	}
 
 	
 	/*
